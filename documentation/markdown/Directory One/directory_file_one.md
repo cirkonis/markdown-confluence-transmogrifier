324bac117dca588fa42e053c12b42cc16d4e47ce
Michael Marigliano <43167596+cirkonis@users.noreply.github.com>
2024-09-26 14:47:12 +0200

    Fixing link url (#12)
    * confluence link url changed
    
    * added some test
    
    * adding some test documentation
    
    * multiple links in a line supported
    
    ---------
    
    Co-authored-by: Michael Marigliano <102356266+mimar126@users.noreply.github.com>

--- documentation/markdown/Directory One/directory_file_one.md
+++ documentation/markdown/Directory One/directory_file_one.md
@@ -3,3 +3,9 @@
 ## TRANSMOGRIFY
 
 This is a line with an image in it ![TRANSMOGRIFY](../../images/transmogrify.jpg)
+
+## Links
+
+Link to [File one](../file_one.md)
+
+Link to [File one](../file_one.md) in the same line as link to [File two](../file_two.md)
--- functions/transmogrify_links.py
+++ functions/transmogrify_links.py
@@ -2,6 +2,35 @@ import re
 import config
 
 
+# def transmogrify_links(line):
+#     """
+#     Processes a line to convert Markdown links to Confluence format using the base URL from the configuration.
+#
+#     Args:
+#         line (str): The line of text from the Markdown file.
+#
+#     Returns:
+#         str: The line with Markdown links converted to Confluence format.
+#     """
+#     confluence_base_url = config.CONFLUENCE_BASE_URL
+#     confluence_space = config.CONFLUENCE_SPACE
+#
+#     # Remove the /rest/api/ part of the base URL and prepare for the new format
+#     confluence_base_url = confluence_base_url.replace("/rest/api/", f"/display/{confluence_space}/")
+#
+#     link_results = re.findall(r'\[(.*?)\]\((?!http|https)(.*?)\)', line)
+#
+#     for link_text, md_link_path in link_results:
+#         confluence_page_title = md_link_path.split('/')[-1].replace('.md', '').replace('-', ' ').replace('_',
+#                                                                                                          ' ').capitalize()
+#         # Construct the new Confluence URL
+#         confluence_url = f"{confluence_base_url}{confluence_page_title.replace(' ', '+')}"
+#
+#         line = re.sub(r'\[.*?\]\((?!http|https).*?\)', f'[{link_text}]({confluence_url})', line)
+#
+#     return line
+
+
 def transmogrify_links(line):
     """
     Processes a line to convert Markdown links to Confluence format using the base URL from the configuration.
@@ -13,12 +42,22 @@ def transmogrify_links(line):
         str: The line with Markdown links converted to Confluence format.
     """
     confluence_base_url = config.CONFLUENCE_BASE_URL
+    confluence_space = config.CONFLUENCE_SPACE
+
+    # Remove the /rest/api/ part of the base URL and prepare for the new format
+    confluence_base_url = confluence_base_url.replace("/rest/api/", f"/display/{confluence_space}/")
+
+    # Find all markdown links in the line
     link_results = re.findall(r'\[(.*?)\]\((?!http|https)(.*?)\)', line)
 
     for link_text, md_link_path in link_results:
-        confluence_page_title = md_link_path.split('/')[-1].replace('.md', '').replace('-', ' ').replace('_',
-                                                                                                         ' ').capitalize()
+        # Generate the Confluence page title
+        confluence_page_title = md_link_path.split('/')[-1].replace('.md', '').replace('-', ' ').replace('_', ' ').capitalize()
+
+        # Construct the new Confluence URL
         confluence_url = f"{confluence_base_url}{confluence_page_title.replace(' ', '+')}"
-        line = re.sub(r'\[.*?\]\((?!http|https).*?\)', f'[{link_text}]({confluence_url})', line)
+
+        # Replace only the current link (specific match) instead of all at once
+        line = line.replace(f'[{link_text}]({md_link_path})', f'[{link_text}]({confluence_url})')
 
     return line
